bb6c69c529261fbbac2f06761f7bacb9e862d7aa
Alimzhanov Ibragim <ballismlife@gmail.com>
2025-02-04 21:39:21 +0500

    Handle form submission

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -8,6 +8,7 @@ import {
   Controller
 } from 'react-hook-form';
 import SimpleMDE from 'react-simplemde-editor';
+import axios from 'axios';
 
 import { Input } from '@/components/ui/input';
 import 'easymde/dist/easymde.min.css';
@@ -18,8 +19,11 @@ type FormValues = {
 };
 
 const TodosPage = () => {
-  const { handleSubmit, control } = useForm<FormValues>();
-  const onSubmit: SubmitHandler<FormValues> = data => console.log(data);
+  const { register, handleSubmit, control } = useForm<FormValues>();
+  const onSubmit: SubmitHandler<FormValues> = async data => {
+    console.log(data);
+    await axios.post('/api/todos', data);
+  };
   const onError: SubmitErrorHandler<FormValues> = errors => console.log(errors);
 
   return (
@@ -33,23 +37,11 @@ const TodosPage = () => {
           onSubmit={handleSubmit(onSubmit, onError)}
           className="flex flex-col gap-[10px]"
         >
-          <Controller
-            name="title"
-            control={control}
-            render={({ field }) => (
-              <Input {...field} placeholder="Add todo title" />
-            )}
-          />
+          <Input {...register('title')} placeholder="Add todo title" />
           <Controller
             name="description"
             control={control}
-            render={({ field }) => (
-              <SimpleMDE
-                {...field}
-                onChange={value => field.onChange(value)}
-                value={field.value}
-              />
-            )}
+            render={({ field }) => <SimpleMDE {...field} />}
           />
           <Input type="submit" value="Add todo" />
         </form>
