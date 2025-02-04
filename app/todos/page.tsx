6d45012c463adadf78d768a794e2cfe492286a10
Alimzhanov Ibragim <ballismlife@gmail.com>
2025-02-04 22:05:26 +0500

    Implement client-side validation

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -5,19 +5,26 @@ import { useForm, SubmitHandler, Controller } from 'react-hook-form';
 import SimpleMDE from 'react-simplemde-editor';
 import axios from 'axios';
 import { toast } from 'sonner';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
 
 import { Input } from '@/components/ui/input';
+import { createTodoSchema } from '@/app/validationSchemas';
 import 'easymde/dist/easymde.min.css';
 
-type FormValues = {
-  title: string;
-  description: string;
-};
+type TodoForm = z.infer<typeof createTodoSchema>;
 
 const TodosPage = () => {
-  const { register, handleSubmit, control } = useForm<FormValues>();
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors }
+  } = useForm<TodoForm>({
+    resolver: zodResolver(createTodoSchema)
+  });
 
-  const onSubmit: SubmitHandler<FormValues> = async data => {
+  const onSubmit: SubmitHandler<TodoForm> = async data => {
     try {
       await axios.post('/api/todos', data);
       console.log(data);
@@ -41,11 +48,17 @@ const TodosPage = () => {
           className="flex flex-col gap-[10px]"
         >
           <Input {...register('title')} placeholder="Add todo title" />
+          {errors.title && (
+            <p className="text-sm text-red-500">{errors.title.message}</p>
+          )}
           <Controller
             name="description"
             control={control}
             render={({ field }) => <SimpleMDE {...field} />}
           />
+          {errors.description && (
+            <p className="text-sm text-red-500">{errors.description.message}</p>
+          )}
           <Input type="submit" value="Add todo" />
         </form>
       </div>
