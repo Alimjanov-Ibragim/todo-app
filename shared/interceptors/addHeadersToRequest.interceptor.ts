6d45f84e05278833d65b2199e4f54329a698b5c6
Alimzhanov Ibragim <ballismlife@gmail.com>
2025-02-05 22:56:26 +0500

    remove unused

--- shared/interceptors/addHeadersToRequest.interceptor.ts
+++
@@ -1,6 +0,0 @@
-import { type InternalAxiosRequestConfig } from 'axios';
-
-export function addHeadersToRequest(config: InternalAxiosRequestConfig) {
-  const token = 'test';
-  config.headers.Authorization = token ? `Bearer ${token}` : undefined;
-}
